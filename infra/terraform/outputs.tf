1c1304c2617ef749226fb8e3be447805fc6c4e43
stackgen-automation <automation@stackgen.com>
2024-09-28 09:47:22 +0000

    Automated commit

---
+++ infra/terraform/outputs.tf
@@ -0,0 +1,71 @@
+
+output "aws_iam_role_hello_kitty_prabha_role_arn" {
+  value = module.appcd_807ec39a-7873-517b-a074-62cea2acc9ed.arn
+  sensitive = false
+}
+
+
+output "aws_iam_role_hello_kitty_prabha_role_name" {
+  value = module.appcd_807ec39a-7873-517b-a074-62cea2acc9ed.name
+  sensitive = false
+}
+
+
+output "aws_s3_hello_kitty_prabha1_s3_arn" {
+  value = module.appcd_5caa565e-76d4-5a21-89e0-ae26b9bb2d2c.arn
+  sensitive = false
+}
+
+
+output "aws_s3_hello_kitty_prabha1_s3_bucket_name" {
+  value = module.appcd_5caa565e-76d4-5a21-89e0-ae26b9bb2d2c.bucket_name
+  sensitive = false
+}
+
+
+output "aws_s3_hello_kitty_prabha1_s3_bucket_website_endpoint" {
+  value = module.appcd_5caa565e-76d4-5a21-89e0-ae26b9bb2d2c.bucket_website_endpoint
+  sensitive = false
+}
+
+
+output "aws_s3_hello_kitty_prabha1_s3_kms_arn" {
+  value = module.appcd_5caa565e-76d4-5a21-89e0-ae26b9bb2d2c.kms_arn
+  sensitive = false
+}
+
+
+output "aws_cloudwatch_log_group_aws_lambda_hello_kitty_prabha_function_logs_arn" {
+  value = module.appcd_bbc4fa7d-2ee2-546d-a02c-92f1a9b615f6.arn
+  sensitive = false
+}
+
+
+output "aws_cloudwatch_log_group_aws_lambda_hello_kitty_prabha_function_logs_name" {
+  value = module.appcd_bbc4fa7d-2ee2-546d-a02c-92f1a9b615f6.name
+  sensitive = false
+}
+
+
+output "aws_lambda_hello_kitty_prabha_function_function_arn" {
+  value = module.appcd_e93b3dbc-378e-501b-9790-00cf75529542.function_arn
+  sensitive = false
+}
+
+
+output "aws_lambda_hello_kitty_prabha_function_function_name" {
+  value = module.appcd_e93b3dbc-378e-501b-9790-00cf75529542.function_name
+  sensitive = false
+}
+
+
+output "aws_lambda_hello_kitty_prabha_function_function_url" {
+  value = module.appcd_e93b3dbc-378e-501b-9790-00cf75529542.function_url
+  sensitive = false
+}
+
+
+output "aws_lambda_hello_kitty_prabha_function_invoke_arn" {
+  value = module.appcd_e93b3dbc-378e-501b-9790-00cf75529542.invoke_arn
+  sensitive = false
+}
